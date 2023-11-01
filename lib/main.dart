9545992020c3eaea062f98f1f0a2406de59dddbf
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-11-01 19:47:33 -0300

    Feat: criando a coluna

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp()); // Chama o Main do App
-}
+void main() => runApp(const MyApp());
 
+/// Main application widget
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'Contador';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const HomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'), //Chamando a outra classe
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState(); //Chamando a outra classe abaixo
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+/// Creating my column
+class HomePage extends StatelessWidget {
+  const HomePage({Key ? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return const Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+            children: [
+                Text(
+                  'Pode Entrar',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                    '0',
+                  style: TextStyle(
+                    fontSize: 100,
+                    color: Colors.white,
+                  ),
+                )
+            ],
+        )
     );
   }
 }
