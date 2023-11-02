bdc877aa5410663a88c7569f608e56d90d66ac64
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-11-01 21:18:09 -0300

    Feat: adicionando imagem no background

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,11 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Contador';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const HomePage(),
-      ),
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:  HomePage(),
     );
   }
 }
@@ -31,65 +26,71 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.black,
         body: Container(
           padding: const EdgeInsets.all(32),
-          child:  Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Pode Entrar',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const Text(
-                '0',
-                style: TextStyle(
-                  fontSize: 100,
-                  color: Colors.white,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.yellow,
-                        fixedSize: const Size(100, 100),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        )
-                    ),
-                    onPressed: decrement,
-                    child: const Text('Saiu',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 64),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: Colors.yellow,
-                        fixedSize: const Size(100, 100),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        )
-                    ),
-                    onPressed: increment,
-                    child: const Text('Entrou',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ], // Column Children
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('/images/Flutter01.jpg'),
+                  fit: BoxFit.cover
               )
-            ], // Column Children
           ),
-        )
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Pode Entrar',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Text(
+                  '0',
+                  style: TextStyle(
+                    fontSize: 100,
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.yellow,
+                          fixedSize: const Size(100, 100),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          )
+                      ),
+                      onPressed: decrement,
+                      child: const Text('Saiu',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 64),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.yellow,
+                          fixedSize: const Size(100, 100),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          )
+                      ),
+                      onPressed: increment,
+                      child: const Text('Entrou',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ], // Column Children
+                )
+              ], // Column Children
+          ),
+        ),
     );
   }
 }
