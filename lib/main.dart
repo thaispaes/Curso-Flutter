c8e720142f36c0c7ccf85e334b0d0ff740ae0476
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-11-01 21:27:00 -0300

    Feat: modificando de Stateless para Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -17,82 +17,91 @@ class MyApp extends StatelessWidget {
 }
 
 /// Creating my column
-class HomePage extends StatelessWidget {
-  const HomePage({Key ? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Container(
-          padding: const EdgeInsets.all(32),
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('/images/Flutter01.jpg'),
-                  fit: BoxFit.cover
-              )
-          ),
-          child: Column(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('/images/Flutter01.jpg'),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode Entrar',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Text(
+                '$count',
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(
-                  'Pode Entrar',
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
+                TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )
+                  ),
+                  onPressed: decrement,
+                  child: const Text('Saiu',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
                   ),
                 ),
-                const Text(
-                  '0',
-                  style: TextStyle(
-                    fontSize: 100,
-                    color: Colors.white,
+                const SizedBox(width: 64),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.yellow,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      )
+                  ),
+                  onPressed: increment,
+                  child: const Text('Entrou',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Colors.yellow,
-                          fixedSize: const Size(100, 100),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
-                          )
-                      ),
-                      onPressed: decrement,
-                      child: const Text('Saiu',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 64),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Colors.yellow,
-                          fixedSize: const Size(100, 100),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
-                          )
-                      ),
-                      onPressed: increment,
-                      child: const Text('Entrou',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ], // Column Children
-                )
               ], // Column Children
-          ),
+            )
+          ], // Column Children
         ),
+      ),
     );
   }
 }
+
+
 
 
--- lib/methods.dart
+++ lib/methods.dart
@@ -1,7 +1,12 @@
+int count = 0;
+
+
 void decrement(){
+  count--;
   print('decrement');
 }
 
 void increment(){
+  count++;
   print('increment');
 }
